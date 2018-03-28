60020d875137198993d02211e9671e3e18124e55
Steven Bell <sebell@stanford.edu>
2018-03-27 19:09:38 -0700

    Deeper hw introspection + parameterization for vivado design
    Design is still missing clk/reset, but otherwise generates

--- README.md
+++ README.md
@@ -3,14 +3,18 @@
 This is one backend designed to be used with f4graph and Halide-HLS.
 
 # Files
+- **hw**: Scripts to build a hardware system which contains an accelerator, cameras, and DisplayPort output
 - **ubuntu**: scripts to build a root filesystem from Ubuntu base.  This is a convenient base system for development, since it makes it easy to install lots of development packages and other utilities without recompiling the whole rootfs in Petalinux.
 - **drivers**: Kernel drivers for pushing images through accelerators created with Halide-HLS.
-- **hwutils**: tcl scripts and other utilities that we've created for debugging and testing hardware.
+- **hwutils**: tcl scripts and other utilities that we've created for debugging and testing FPGA designs.
 - **petalinux**: petalinux project for building all the boot collateral.
 - **doc**: various bits of documentation and notes.
 
 # Supported devices
-At the moment, we support only the UltraZed-EG.
+At the moment, this supports only the UltraZed-EG with the PCIe carrier card.
+
+Currently supported version of Vivado is 2017.2.
 
 # Build flow
+1. (Optional) Create a hardware accelerator with Halide, and ensure that HLS has packaged it as an IP core.
 
--- parameterize.py
+++ parameterize.py
@@ -20,8 +20,8 @@ if len(argv) < 3:
   print "  DSTFILE is the output file, possibly in a different location."
   exit()
 
-if not argv[1].endswith('.yml'):
-  print "hw config file doesn't end with .yml - is this a mistake?"
+# if not argv[1].endswith('.yml'):
+#   print "hw config file doesn't end with .yml - is this a mistake?"
 
 paramFile = argv[1]
 templateFiles = argv[2:]
@@ -34,16 +34,16 @@ params['cmdline'] = ' '.join(argv)
 params['date'] = time.asctime()
 
 # Calculate some parameters from the file to make things easier inside
-# the templates
-if not params.has_key('taps') or params['taps'] == None:
-  params['taps'] = []
-
-offsets = [0]
-for t in params['taps']:
-  offsets.append(offsets[-1] + int(t['width'])*int(t['height'])*int(t['depth']))
-
-params['tapoffsets'] = offsets
-params['regwidth'] = int(params['controlregs']) + offsets[-1]
+# # the templates
+# if not params.has_key('taps') or params['taps'] == None:
+#   params['taps'] = []
+# 
+# offsets = [0]
+# for t in params['taps']:
+#   offsets.append(offsets[-1] + int(t['width'])*int(t['height'])*int(t['depth']))
+# 
+# params['tapoffsets'] = offsets
+# params['regwidth'] = int(params['controlregs']) + offsets[-1]
 
 # Iterate through all the files and run the template engine
 for f in templateFiles:
