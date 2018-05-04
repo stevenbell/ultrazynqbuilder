b791a3e8d6a4dd0b9b0b7af524964032af7f99b2
Gedeon Nyengele <nyengele@stanford.edu>
2018-05-04 09:57:01 -0700

    dma driver added

--- drivers/ioctl_cmds.h
+++ drivers/ioctl_cmds.h
@@ -4,12 +4,17 @@
 #include <linux/ioctl.h>
 #include "ubuffer.h"
 
-#define CMA_MAGIC	'Z'
-#define GET_BUFFER 	_IOWR(CMA_MAGIC, 0x20, struct UBuffer *)	 // Get an unused buffer
-#define GRAB_IMAGE 	_IOWR(CMA_MAGIC, 0x21, struct UBuffer *)	 // Acquire image from camera
-#define FREE_IMAGE 	_IOWR(CMA_MAGIC, 0x22, struct UBuffer *)	 // Release buffer
+/* CMA BUFFER */
+#define CMA_MAGIC		'Z'
+#define GET_BUFFER 		_IOWR(CMA_MAGIC, 0x20, struct UBuffer *)	 // Get an unused buffer
+#define GRAB_IMAGE 		_IOWR(CMA_MAGIC, 0x21, struct UBuffer *)	 // Acquire image from camera
+#define FREE_IMAGE 		_IOWR(CMA_MAGIC, 0x22, struct UBuffer *)	 // Release buffer
 #define PROCESS_IMAGE 	_IOWR(CMA_MAGIC, 0x23, struct UBuffer *)	 // Push to stencil path
 #define PEND_PROCESSED 	_IOWR(CMA_MAGIC, 0x24, struct UBuffer *)	 // Retreive from stencil path
-#define READ_TIMER 	_IOWR(CMA_MAGIC, 0x25, struct UBuffer *)	 // Retreive hw timer count
+#define READ_TIMER		_IOWR(CMA_MAGIC, 0x25, struct UBuffer *)	 // Retreive hw timer count
+
+/* DMA */
+#define XILDMA_MAG		'Z'
+#define GRAB_IMAGE		_IOWR(XILDMA_MAG, 0x40, unsigned long *)
 
 #endif /* _IOCTL_CMDS_H */
