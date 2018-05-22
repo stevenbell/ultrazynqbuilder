6a4c4f2a9399bb342a880a34276c2e2fbcacd6f7
Keyi <keyi@stanford.edu>
2018-05-21 21:46:12 -0700

    allow annotating specific output channels

--- extractparams.py
+++ extractparams.py
@@ -1,6 +1,6 @@
 # Read the user parameters file (usually hwconfig.user) and extract
 # parameters for all of the user-defined modules specified there.
-
+from __future__ import print_function
 import os
 import yaml
 from lxml import etree
@@ -17,10 +17,10 @@ for module in params['hw']:
     continue
 
   if not os.path.isdir(module['path']):
-    print "[Error] Couldn't find IP for %s in %s!" % (module['name'], module['path'])
+    print("[Error] Couldn't find IP for %s in %s!" % (module['name'], module['path']))
     continue
 
-  print "Extracting parameters for " + module['name']
+  print("Extracting parameters for", module['name'])
   module['streams'] = [] # Start empty list of data streams
 
   desc = etree.parse(open(module['path']+'/component.xml'))
@@ -50,25 +50,38 @@ for module in params['hw']:
     if len(streamdepths) == 1:
       stream['depth'] = int(streamdepths[0])
     else:
-      print "[Warning] Failed to get stream depth for stream %s" % stream['name']
+      print("[Warning] Failed to get stream depth for stream", stream['name'])
 
     module['streams'].append(stream)
 
 # second pass
 for module in params["hw"]:
   if module["type"] == "hls":
+    input_types = ["dma", "csi"]
     # we need to collect all the DMA's that are pointing to it
     input_dmas = []
+    specified_dmas = []
     for dma in params["hw"]:
-      if dma["type"] != "dma" or dma["outputto"] != module["name"]:
+      if dma["type"] not in input_types:
         continue
       # we have found an input dma channel
-      input_dmas.append(dma)
+      if dma["outputto"] == module["name"]:
+        input_dmas.append(dma)
+      elif "." in dma["outputto"] and dma["outputto"].split(".")[0] == module["name"]:
+        specified_dmas.append(dma)
+      else:
+        print("[Warning] Mismatched channel name. Expect module name", module["name"],
+              "got", dma["outputto"])
     input_streams = [entry for entry in module["streams"]
                      if entry["type"] == "input"]
     # we need to make sure that the input list matches with the HLS IP
-    assert(len(input_dmas) == len(input_streams))
+    assert(len(input_dmas + specified_dmas) == len(input_streams))
+    # remove the input_streams that's been specified already
+    for dma in specified_dmas:
+      input_streams = [entry for entry in input_streams
+                       if entry["name"] != dma["outputto"].split(".")[-1]]
     # rename the outputto property
+    # also double check if the user already specifies the connections
     for i in range(len(input_dmas)):
       # updating the object by reference
       # the format will be hls_module.arg_#
