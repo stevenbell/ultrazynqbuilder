4fa2d493cf0ab7d5e375eeed05bb367367f84508
Keyi Zhang <keyi@stanford.edu>
2018-06-24 01:58:08 -0700

    add indexing support in device tree

---
+++ dtconfig.py
@@ -0,0 +1,126 @@
+import yaml
+import sys
+
+"""
+Example usage:
+
+$> python dtconfig.py path/to/hwconfig.user [output_dt_overlay_filename]
+"""
+
+####### PARAMETER DEFINITIONS ###############
+
+# value of "compatible" property for hls nodes
+hls_compatible_string = "hls-target"
+
+# value of "compatible" property for dmas
+dma_compatible_string = "hls-dma"
+
+# name of hls node's property that references input dmas
+prop_name_dmas = "dmas"
+
+# name of dma node's property that references parent hls node
+prop_name_hls_ref = "hls-node"
+
+# name of the property that contains device node's name
+prop_name_node_name = "hw-name"
+
+######## END OF PARAMETE DEFINITIONS ########
+
+# check command-line args
+usage_prompt = "usage: python dtconfig.py <path/to/hwconfig_file> [output_dt_overlay_filename]"
+if(len(sys.argv) < 2):
+	print(usage_prompt)
+	exit()
+
+cfg_file_path = sys.argv[1]
+
+if (len(sys.argv) == 3):
+	dts_file_path = sys.argv[2]
+else:
+	dts_file_path = "system-user-overlay.dtsi"
+
+# read yaml file and convert to dictionary
+with open(cfg_file_path, 'r') as cfg_file:
+	cfg = yaml.load(cfg_file)
+
+if 'hw' not in cfg:
+	sys.exit("[dtconfig]: the 'hw' node not found in the provided hardware configuration file")
+
+overlay = dict()
+
+# overlay initialization
+for hw_node in cfg['hw']:
+	node_type = hw_node['type']
+	node_name = hw_node['name']
+
+	if (node_type ==  'dma' or node_type == 'hls'):
+			overlay[node_name] = dict()
+			overlay[node_name]['definition'] = hw_node
+
+			if(node_type == 'dma'):
+				overlay[node_name]['direction'] = 0
+				overlay[node_name]['hls-node'] = ""
+			else: # node_type == 'hls'
+                                # not using set because we want to perserve order
+				overlay[node_name]['dmas'] = []
+
+# filling in overlay info
+for key in overlay:
+	node = overlay[key]['definition']
+	node_type = node['type']
+	node_name = node['name']
+
+	if(node_type == 'dma'):
+		if 'outputto' in node:
+                        overlay[node_name]['direction'] += 1
+                        outputto = node["outputto"].split('.')[0]
+                        overlay[node_name]['hls-node'] = outputto
+                        if node_name not in overlay[outputto]['dmas']:
+                            overlay[outputto]['dmas'].append(node_name)
+	else:
+		if node['outputto'] in overlay and overlay[node['outputto']]['definition']['type'] == 'dma':
+                    if node['outputto'] not in overlay[node_name]['dmas']:
+                            overlay[node_name]['dmas'].append(node['outputto'])
+                            overlay[node['outputto']]['hls-node'] = node_name
+                            overlay[node['outputto']]['direction'] += 2
+
+
+# creating dt overlay
+dt_overlay = ""
+index = 0
+for key in sorted(overlay.iterkeys()):
+	dt_overlay += "&" + key + " {"
+	dt_overlay += "\n\t" + prop_name_node_name + " = " + "\"" + overlay[key]['definition']['name'] + "\";"
+
+	if overlay[key]['definition']['type'] == 'dma':
+		dt_overlay += "\n\tcompatible = \"" + dma_compatible_string + "\";"
+		dt_overlay += "\n\tdirection = <" + str(overlay[key]['direction']) + ">;"
+		dt_overlay += "\n\t" + prop_name_hls_ref + " = <&" + overlay[key]['hls-node'] + ">;"
+                # since all the dmas have input channel (we only have one output). dma input channel
+                # sequence is simple
+                dt_overlay += "\n\tdma-index = <" + str(index) + ">;"
+                index += 1
+	else:
+		dt_overlay += "\n\tcompatible = \"" + hls_compatible_string + "\";"
+		dt_overlay += "\n\tgpio = <&axi_gpio_1>;"
+		#input dmas
+		if len(overlay[key]['dmas']) == 0:
+			dt_overlay += "\n\t" + prop_name_dmas + " = <empty>;"
+		else:
+			dt_overlay += "\n\t" + prop_name_dmas + " = "
+			for counter, value in enumerate(overlay[key]['dmas']):
+				dt_overlay += "<&" + value + ">"
+				if(counter < len(overlay[key]['dmas']) - 1):
+					dt_overlay += ", "
+				else:
+					dt_overlay += ";"
+
+	dt_overlay += "\n};\n"
+
+with open(dts_file_path, 'w+') as dts_file:
+	dts_file.write(dt_overlay)
+
+cfg_file.close()
+dts_file.close()
+
+print("[dtconfig] Device-tree overlay written to %s" % (dts_file_path))
--- extractparams.py
+++ extractparams.py
@@ -1,10 +1,15 @@
 # Read the user parameters file (usually hwconfig.user) and extract
 # parameters for all of the user-defined modules specified there.
-
+from __future__ import print_function
 import os
 import yaml
+import sys
 from lxml import etree
 #from IPython import embed
+
+def is_power2(num):
+    return num != 0 and ((num & (num - 1)) == 0)
+
 
 inpath = 'hwconfig.user'
 outpath = 'hwconfig.all'
@@ -17,10 +22,10 @@ for module in params['hw']:
     continue
 
   if not os.path.isdir(module['path']):
-    print "[Error] Couldn't find IP for %s in %s!" % (module['name'], module['path'])
+    print("[Error] Couldn't find IP for %s in %s!" % (module['name'], module['path']))
     continue
 
-  print "Extracting parameters for " + module['name']
+  print("Extracting parameters for", module['name'])
   module['streams'] = [] # Start empty list of data streams
 
   desc = etree.parse(open(module['path']+'/component.xml'))
@@ -50,11 +55,112 @@ for module in params['hw']:
     if len(streamdepths) == 1:
       stream['depth'] = int(streamdepths[0])
     else:
-      print "[Warning] Failed to get stream depth for stream %s" % stream['name']
+      print("[Warning] Failed to get stream depth for stream", stream['name'])
 
     module['streams'].append(stream)
 
-outfile = open(outpath, 'w')
+# second pass
+for module in params["hw"]:
+  if module["type"] == "hls":
+    input_types = ["dma", "csi"]
+    # we need to collect all the DMA's that are pointing to it
+    input_dmas = []
+    specified_dmas = []
+    for dma in params["hw"]:
+      if dma["type"] not in input_types:
+        continue
+      # we have found an input dma channel
+      if dma["outputto"] == module["name"]:
+        input_dmas.append(dma)
+      elif "." in dma["outputto"] and dma["outputto"].split(".")[0] == module["name"]:
+        specified_dmas.append(dma)
+      else:
+        print("[Warning] Mismatched channel name. Expect module name", module["name"],
+              "got", dma["outputto"])
+    input_streams = [entry for entry in module["streams"]
+                     if entry["type"] == "input"]
+    # we need to make sure that the input list matches with the HLS IP
+    assert(len(input_dmas + specified_dmas) == len(input_streams))
+    # remove the input_streams that's been specified already
+    for dma in specified_dmas:
+      input_streams = [entry for entry in input_streams
+                       if entry["name"] != dma["outputto"].split(".")[-1]]
+    # rename the outputto property
+    # also double check if the user already specifies the connections
+    for i in range(len(input_dmas)):
+      # updating the object by reference
+      # the format will be hls_module.arg_#
+      input_dmas[i]["outputto"] += "." + input_streams[i]["name"]
+
+# thid pass to see if there is any input only dma
+for module in params["hw"]:
+    if module["type"] == "dma":
+        out_connected = False
+        for m in params["hw"]:
+            if m["type"] == "hls" and m["outputto"] == module["name"]:
+                out_connected = True
+                break
+        module["out_connected"] = out_connected
+
+# fourth pass to properly setup dma width
+for module in params["hw"]:
+    if module["type"] == "dma":
+        # search connected hls module to see its depth
+        for m in params["hw"]:
+            if m["type"] == "hls" and "outputto" in m and \
+                    m["outputto"] == module["name"]:
+                # find the output stream
+                s = None
+                for stream in m["streams"]:
+                    if stream["type"] == "output":
+                        s = stream
+                        break
+                if s is None:
+                    raise Exception("no output found in " + m["name"])
+                depth = s["depth"]
+                if not is_power2(depth):
+                    depth = 2
+                module["s2mm_width"] = depth * 8
+
+            out_args = module["outputto"].split(".")
+            module_name = out_args[0]
+            stream_name = out_args[1]
+            if m["name"] == module_name:
+                for stream in m["streams"]:
+                    if stream["name"] == stream_name:
+                        s = stream
+                        break
+                depth = s["depth"]
+                if not is_power2(depth):
+                    depth = 2
+                module["mm2s_width"] = depth * 8
+        # sanity check
+        if (module["out_connected"] and "s2mm_width" not in module) or \
+                ("mm2s_width" not in module):
+            print(module)
+            raise Exception("corrupted hwconfig file")
+
+# last pass to print out the dma channel order without modification
+dmas = []
+for module in params["hw"]:
+    if module["type"] == "dma":
+        # assumes only one HLS module
+        dmas.append((module["name"], module["out_connected"],
+                     module["outputto"].split(".")[-1]))
+# notice that the ordering of dma channel loading is
+# dma0 (in + out) -> dma1 (in) -> dma2 (in)
+# therefore the default loading order is
+# dma0 -> dma1 -> dma2. we should print this out and let user modify generated
+# dtoverlay file
+if not dmas[0][1]:
+    # the first one should be output channel. so somehow we messed up
+    print("[WARNING]: corrupted dma channel sequence", file=sys.stderr)
+print("Default input dma channel loading sequence in kernel:")
+for index, dma in enumerate(dmas):
+    print("[{0}]: {1} -> {2}".format(index, dma[0], dma[2]))
+print("Please modify dtoverlay accordingly")
+
+outfile = open(outpath, 'w+')
 outfile.write(yaml.dump(params))
 outfile.close()
 
