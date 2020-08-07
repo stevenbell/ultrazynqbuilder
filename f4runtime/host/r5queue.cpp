fbf7023df6559dc008048c8195038a575ff36a7f
Steven Bell <botsnlinux@gmail.com>
2020-08-07 19:17:39 +0000

    APU-side updates to pass request results back

--- f4runtime/host/r5queue.cpp
+++ f4runtime/host/r5queue.cpp
@@ -7,43 +7,10 @@
 #include <metal/io.h>
 #include <metal/alloc.h>
 #include "ttc_clock.h"
+#include "mailbox.h"
 
 #define BUS_NAME        "platform"
 #define SHM_DEV_NAME    "3e800000.shm"
-
-// WARNING: the size of an enum is not defined by the C standard, and may
-// differ across platforms.
-
-typedef enum {
-  CAMERA0,
-  CAMERA1,
-  CAMERA2,
-  CAMERA3,
-  FLASH0,
-  FLASH1,
-  FLASH2,
-  FLASH3,
-  NO_DEVICE
-} ReqDevice;
-
-typedef struct {
-  uint32_t exposure;
-} CameraParams;
-
-typedef struct {
-  uint32_t duration;
-} FlashParams;
-
-typedef struct {
-  ReqDevice device;
-  Time time;
-  union{
-    CameraParams camParams;
-    FlashParams flashParams;
-  };
-} Request;
-
-const int QUEUE_LEN = 10;
 
 int main(int argc, char* argv[])
 {
@@ -67,13 +34,11 @@ int main(int argc, char* argv[])
     return(-ENODEV);
 	}
 
-  /* layout:
-   * 0x00: master pointer
-   * 0x04: slave pointer
-   * 0x08: buffers
-   */
-  uint32_t master = 0; // Our count (id of last packet completed)
-  metal_io_write32(io, 0, master); // Initialize the master count
+  uint32_t a2r_head = 0; // Our count (location of last Request sent)
+  metal_io_write32(io, A2R_HEAD, a2r_head); // Initialize the a2r_head count
+
+  uint32_t r2a_tail = 0; // Our count (id of last packet completed)
+  //metal_io_write32(io, R2A_TAIL, r2a_tail);
 
   printf("Enter a number for the exposure, q to quit.\n");
   while(true){ // Loop until we break out
@@ -89,44 +54,60 @@ int main(int argc, char* argv[])
     }
     uint32_t param = (uint32_t)strtol(command, NULL, 10);
 
-    uint32_t slave = metal_io_read32(io, 4); // Current slave pointer
+    uint32_t tail = metal_io_read32(io, A2R_TAIL);
 
     // Move to the next slot, and check that we're not going to overflow the
     // ring buffer
-    master = (master + 1) % QUEUE_LEN;
-    if(master == slave){
+    a2r_head = (a2r_head + 1) % QUEUE_LEN;
+    if(a2r_head == tail){
       printf("Message queue is full!\n");
       break; // Just die, for now
     }
 
     // Send a message to the R5
     Request req;
-/*
-    req.device = CAMERA0;
+
+    req.device = SHUTTERBUTTON;
     req.camParams.exposure = param;
     req.time = ttc_clock_now() + 5e5; // 500ms from now
-    printf("\nRequesting exposure %d at time %ld\n", req.params.exposure, req.time);
-*/
+    printf("\nRequesting exposure %d on dev %d at time %ld\n", req.camParams.exposure, req.device, req.time);
+
+/*
     req.device = FLASH0;
     req.flashParams.duration = param;
     req.time = ttc_clock_now() + 5e5; // 500ms from now
     printf("\nRequesting %d us long flash at time %ld\n", req.flashParams.duration, req.time);
 
-    metal_io_block_write(io, 0x08 + master*(sizeof(Request)), &req, sizeof(Request));
-
+*/
+    metal_io_block_write(io, A2R_BUFFER_BASE + a2r_head*(sizeof(Request)), &req, sizeof(Request));
 /*
     // HACK: make the same request on the other camera
-    master = (master + 1) % QUEUE_LEN;
-    if(master == slave){
+    a2r_head = (a2r_head + 1) % QUEUE_LEN;
+    if(a2r_head == tail){
       printf("Message queue is full!\n");
       break; // Just die, for now
     }
     req.device = CAMERA1;
-    metal_io_block_write(io, 0x08 + master*(sizeof(Request)), &req, sizeof(Request));
+    metal_io_block_write(io, A2R_BUFFER_BASE + a2r_head*(sizeof(Request)), &req, sizeof(Request));
     // END HACK
 */
 
-    metal_io_write32(io, 0, master); // Update the master count
+    metal_io_write32(io, A2R_HEAD, a2r_head); // Update the a2r_head count
+
+
+    // Now check for return metadata (completed requests)
+    uint32_t head = metal_io_read32(io, R2A_HEAD);
+
+    while(r2a_tail != head){
+      r2a_tail = (r2a_tail + 1) % QUEUE_LEN;
+      Request req;
+      int ok = metal_io_block_read(io, R2A_BUFFER_BASE + r2a_tail*(sizeof(Request)), &req, sizeof(Request));
+      if(ok){
+        printf("received return %u on dev %d at time %lu\n", r2a_tail, req.device, req.time);
+      }
+
+      metal_io_write32(io, R2A_TAIL, r2a_tail);
+    }
   }
 
   metal_device_close(shm_dev);
